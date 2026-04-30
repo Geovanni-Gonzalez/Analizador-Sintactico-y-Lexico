117fa5ca9dd0a39508184a2690b7a4c5351d17e2
OwenTP27 <125586153+OwenTP27@users.noreply.github.com>
2026-04-29 20:33:30 -0600

    Add files via upload

--- programa/src/main/java/Main.java
+++ programa/src/main/java/Main.java
@@ -14,21 +14,18 @@ public class Main {
                 System.out.println("Leyendo desde teclado (Ctrl+D para terminar):");
                 reader = new java.io.InputStreamReader(System.in);
             }
- 
-            // 2. Crear lexer
+
             MiLexer lexer = new MiLexer(reader);
  
-            // 3. Crear parser (CUP)
             parser p = new parser(lexer);
  
-            // 4. Ejecutar análisis
             p.parse();
  
-            // 5. Resultado final
             if (p.numErrores > 0) {
-                System.err.println("\nAnálisis finalizado con " + p.numErrores + " error(es).");
+                System.err.println("\nEl archivo fuente no puede ser generado por la gramática.");
+                System.err.println("Se encontraron " + p.numErrores + " error(es).");
             } else {
-                System.out.println("Análisis completado sin errores.");
+                System.out.println("El archivo fuente puede ser generado por la gramática.");
             }
  
         } catch (Exception e) {
@@ -38,3 +35,4 @@ public class Main {
     }
 }
  
+
